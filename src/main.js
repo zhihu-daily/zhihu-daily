0c5c73ebe627f5ee47c17f2cd4e32f56413aefe7
MikeBison <356981553@qq.com>
2017-03-05 00:59:52 +0800

    router没搞定

--- src/main.js
+++ src/main.js
@@ -4,30 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-import index from './components/index/index.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
-/* eslint-disable no-unused-vars */
-const routes = [
-  {
-    path: '/',
-    components: index
-  }
-]
-
-let router = new VueRouter({
-  routes,
-  linkActiveClass: 'active'
-})
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
-  template: '<App/>',
-  components: { App }
+  render (h) {
+    return h(App)
+  }
 }).$mount('#app')
